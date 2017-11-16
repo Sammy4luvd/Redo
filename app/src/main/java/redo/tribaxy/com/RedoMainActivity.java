93242a9f1ad2b53491ba4924c54f395ead30cf40
Dalafiari Samuel <sammy4luvd@gmail.com>
2017-11-16 14:22:46 +0100

    first commit

---
+++ app/src/main/java/redo/tribaxy/com/RedoMainActivity.java
@@ -0,0 +1,13 @@
+package redo.tribaxy.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RedoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redo_main);
+    }
+}
